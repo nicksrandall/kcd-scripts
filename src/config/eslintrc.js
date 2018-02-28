855e764f5d7b08fb932320b65182e1b83af18321
Nick Randall <nicksrandall@gmail.com>
2018-02-28 12:16:15 -0500

    adding a few things

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -1,11 +1,19 @@
 const {ifAnyDep} = require('../utils')
 
 module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
   env: {
     node: true,
     browser: true,
     jest: true,
   },
+  plugins: ['react'],
   extends: [
     'eslint:recommended',
     ifAnyDep('react', 'plugin:react/recommended'),
